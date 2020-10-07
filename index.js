0223f352e2be1f571fca21628e862c7ecb77f4e8
RomanSoroka85 <patriotroma9@gmail.com>
2020-10-07 23:08:17 +0300

    hw8

---
+++ index.js
@@ -0,0 +1,49 @@
+import gallery from "./gallery-items.js";
+console.log(gallery);
+
+const ulGallery = document.querySelector(".js-gallery");
+console.log(ulGallery);
+const lightBox = document.querySelector(".js-lightbox");
+console.log(lightBox);
+const lightBoxButton = document.querySelector(".lightbox__button");
+const lightBoxImg = document.querySelector(".lightbox__image");
+gallery.forEach((object, index) => {
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.setAttribute("src", object.preview);
+  img.setAttribute("data-sourse", object.original);
+  img.setAttribute("alt", object.description);
+  img.setAttribute("data-index", index);
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.append(img);
+  const item = document.createElement("li");
+  item.classList.add("gallery__item");
+  
+  item.append(link);
+  ulGallery.append(item);
+  // console.log(item);
+});
+
+ulGallery.addEventListener("click", (event) => {
+  const newSrc = event.target.dataset.sourse;
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+lightBox.classList.add("is-open");
+  lightBoxImg.src = newSrc;
+  window.addEventListener("keydown", handleEccape);
+});
+const closeLightBox = () => {
+  lightBox.classList.remove("is-open");
+  lightBoxImg.src = "";
+  window.removeEventListener("keydown", handleEccape);
+  window.removeEventListener("keydown", handleScroll);
+};
+
+lightBoxButton.addEventListener("click", closeLightBox);
+function handleEccape(event) {
+  if (event.code === "Escape") {
+    closeLightBox();
+  }
+}
